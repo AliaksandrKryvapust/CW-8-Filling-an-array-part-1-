4c720da9c09448e4974744ce2e8747435f90e3b9
AliaksandrKryvapust <alexandrkryvopust@gmail.com>
2022-06-01 10:53:27 +0400

    Добавьте файлы проекта.

---
+++ CW 8 Filling an array (part 1)/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <vector>
+#include<numeric>
+using namespace std;
+
+vector<int> arr(const int n = 0) {
+	vector<int> result(n);
+	iota(result.begin(), result.end(), 0);
+	return result;
+}
+int main() {
+	int n = 0;
+	return 0;
+}
